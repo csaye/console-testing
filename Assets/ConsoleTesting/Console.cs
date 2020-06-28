f55cfff02bac8361e9fd1855d4a7cb18d18f5121
Cooper Saye <coopersaye@gmail.com>
2020-06-27 19:16:55 -0700

    Create basic test commands.

--- Assets/ConsoleTesting/Console.cs
+++ Assets/ConsoleTesting/Console.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace ConsoleTesting
@@ -18,6 +19,15 @@ namespace ConsoleTesting
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Slash))
+            {
+                if (!consoleActive)
+                {
+                    consoleActive = true;
+                    UpdateConsoleActive();
+                }
+            }
+
             if (Input.GetKeyDown("escape"))
             {
                 consoleActive = !consoleActive;
@@ -41,11 +51,47 @@ namespace ConsoleTesting
             canvasGroup.alpha = consoleActive ? 1 : 0;
             canvasGroup.interactable = consoleActive;
             canvasGroup.blocksRaycasts = consoleActive;
+
+            if (consoleActive)
+            {
+                inputField.ActivateInputField();
+            }
         }
 
         private void RunCommand(string command)
         {
-        
+            if (!command.StartsWith("/"))
+            {
+                Debug.LogError("Command " + command + " does not start with slash.");
+                return;
+            }
+
+            string primaryPhrase;
+
+            if (!command.Contains(" "))
+            {
+                primaryPhrase = command.Substring(1);
+            }
+            else
+            {
+                primaryPhrase = command.Substring(1, command.IndexOf(" ") - 1);
+            }
+
+            switch (primaryPhrase)
+            {
+                case "say":
+                    if (command.Contains(" ")) Debug.Log(command.Substring(command.IndexOf(" ")));
+                    break;
+                case "shout":
+                    if (command.Contains(" ")) Debug.Log(command.Substring(command.IndexOf(" ")).ToUpper());
+                    break;
+                case "double":
+                    if (command.Contains(" ")) try { Debug.Log(2 * Int32.Parse(command.Substring(command.IndexOf(" ")))); } catch { Debug.LogError(command.Substring(command.IndexOf(" ")) + " is an invalid number."); }
+                    break;
+                default:
+                    Debug.LogError("Command " + primaryPhrase + " not found.");
+                    break;
+            }
         }
     }
 }
