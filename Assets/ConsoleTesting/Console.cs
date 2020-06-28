85b84e0d65f38e0d8ced0120b6dd457b6aae3572
Cooper Saye <coopersaye@gmail.com>
2020-06-27 18:22:43 -0700

    Implement basic console functionality.

---
+++ Assets/ConsoleTesting/Console.cs
@@ -0,0 +1,51 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ConsoleTesting
+{
+    public class Console : MonoBehaviour
+    {
+        [Header("References")]
+        public CanvasGroup canvasGroup;
+        public TMP_InputField inputField;
+
+        private bool consoleActive;
+
+        private void Start()
+        {
+            UpdateConsoleActive();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown("escape"))
+            {
+                consoleActive = !consoleActive;
+                UpdateConsoleActive();
+            }
+
+            if (Input.GetKeyDown("return"))
+            {
+                if (consoleActive)
+                {
+                    RunCommand(inputField.text);
+                    inputField.text = "";
+                    consoleActive = false;
+                    UpdateConsoleActive();
+                }
+            }
+        }
+
+        private void UpdateConsoleActive()
+        {
+            canvasGroup.alpha = consoleActive ? 1 : 0;
+            canvasGroup.interactable = consoleActive;
+            canvasGroup.blocksRaycasts = consoleActive;
+        }
+
+        private void RunCommand(string command)
+        {
+        
+        }
+    }
+}
